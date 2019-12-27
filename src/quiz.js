32e891b69ed55124a6221a3ca1c4e701f3ade2c2
Nima Soufiani <n_soufiani@hotmail.com>
2019-12-27 18:42:17 +0000

    improved code readability

--- src/quiz.js
+++ src/quiz.js
@@ -1,28 +1,27 @@
-var question = 0;
-var ansSelected = null;
-var mulAnsSelected = [];
-var data = null; // set initial data to null
-var resultData = null;
-var points = 0;
-var pointsPerc = 0;
-var ansData = [];
-var timer = 200;
-var enSelect = false;
+let question = 0; //the question we are on
+let ansSelected = null;
+let mulAnsSelected = [];
+let data = null; // set initial data to null
+let resultData = null;
+let points = 0;
+let pointsPerc = 0;
+let ansData = [];
+let timer = 200;
+let enSelect = false; //A flag to hide confirm answer button if user tries to select another answer during the duration of the timer.
 
-var answerConfirm = function() {
-  //check if answer is a true or false answer. if yes, switch 0 int to true and 1 int to false.
-  if (data.questions[question].question_type === "truefalse") {
-    if (ansSelected === 0) {
-      ansSelected = true;
-    } else {
-      ansSelected = false;
-    }
-  }
-
-  if (data.questions[question].question_type === "mutiplechoice-multiple") {
-    ansSelected = mulAnsSelected.map(function(str) {
-      return Number(str);
-    });
+let answerConfirm = function() {
+  switch (data.questions[question].question_type) {
+    //ansSelected to be true or false if question type is a true or false question.
+    case "truefalse":
+      ansSelected = !Boolean(ansSelected);
+      break;
+    //below case will returns an array of numbers depending on the amount of answers selected.
+    case "mutiplechoice-multiple":
+      ansSelected = mulAnsSelected.map(function(str) {
+        return Number(str);
+      });
+    default:
+      break;
   }
 
   //the below if else statement checks for correct answer, displays result to user and pushes the result to results array
@@ -30,39 +29,39 @@ var answerConfirm = function() {
     JSON.stringify(ansSelected) ===
     JSON.stringify(data.questions[question].correct_answer)
   ) {
-    $(".confirm").hide(); //hide the confirm button
-    $(".result")
-      .show()
-      .removeClass()
-      .addClass("result success");
-    points += data.questions[question].points; //add to total points
-    console.log(`Points: ${points}`);
-    $('<p id="userres" >Correct answer! Well done :)</p>').appendTo(".result"); //display correct answer text to user
-
-    setTimeout(nextQuestion, timer); //call nextquestion() in 3 seconds
+    hideConfirmButtonShowResult("success", "Correct answer! Well done :)");
   } else {
-    $(".confirm").hide(); //hide button
-    $(".result")
-      .show()
-      .removeClass()
-      .addClass("result fail");
-    $('<p id="userres">Incorrect answer :(</p>').appendTo(".result"); //display incorrect answer text to user
-
-    setTimeout(nextQuestion, timer); //call nextquestion() in 3 seconds
+    hideConfirmButtonShowResult("fail", "Incorrect answer :(");
   }
 };
 
-var nextQuestion = function() {
-  enSelect = false;
-  ansSelected = null;
-  mulAnsSelected = [];
+let hideConfirmButtonShowResult = function(result, message) {
+  $(".confirm").hide(); //hide the confirm button
+  $(".result")
+    .show() //show result
+    .removeClass() //remove any existing css
+    .addClass(`result ${result}`); //add success css
+  $(`<p id="userres" >${message}</p>`).appendTo(".result"); //display correct answer
+  switch (result) {
+    case "success":
+      points += data.questions[question].points; //add to total points
+      break;
+    default:
+      break;
+  }
+  setTimeout(setNextQuestion, timer); //call nextquestion() in 3 seconds
+};
+
+let setNextQuestion = function() {
+  enSelect = false; //enables user to select answer.
+  ansSelected = null; //current answer removed and set to null
+  mulAnsSelected = []; //current multiple choice answer set to empty array
   question += 1; //set question integer so we call correct question
   if (question < data.questions.length) {
-    $("#userres").remove(); //set results text to empty string
-
+    $("#userres").remove(); //removes results text
+    $(".result").hide(); //hides previous result
     $(".quesImage").attr("src", data.questions[question].img); //sets image
     $("#question").text(question + 1 + ") " + data.questions[question].title); //sets question
-
     setAnswers();
   } else {
     $(".result").remove(); //set results text to empty string
@@ -70,95 +69,92 @@ var nextQuestion = function() {
   }
 };
 
-var setAnswers = function() {
+let setAnswers = function() {
   $(".quesForm").remove();
   $(".quesList").remove(); //first remove the current answers.
-  if (data.questions[question].question_type === "truefalse") {
-    var trueFalseList = $("form.quesForms");
-    var liTrue = $(
-      '<label class="quesList" ><input id=0 type="radio" name="radio">True</label>'
-    ).appendTo(trueFalseList);
-
-    var liFalse = $(
-      '<label class="quesList" ><input id=1 type="radio" name="radio">False</label>'
-    ).appendTo(trueFalseList);
-  } else if (
-    data.questions[question].question_type === "mutiplechoice-single"
-  ) {
-    ansData = data.questions[question].possible_answers; // store answers array
-
-    var aList = $("form.quesForms");
-    //use the each method to iterate through the answer array variable, ansData
-    $.each(ansData, function(index, value) {
-      //we create the <li></li> element with a class and an attribute and append it to <ul></ul> which we identify through the class quesLists
-      var li = $("<label/>")
-        .addClass("quesList")
-        .appendTo(aList);
-
-      //we create the <input> element with 2 attributes and append it to <li></li> and then add the anwer after the <input> element.
-      var input = $("<input/>")
-        .attr("type", "radio")
-        .attr("name", "radio")
-        .attr("id", value.a_id)
-        .appendTo(li)
-        .after(value.caption);
-    });
-  } else if (
-    data.questions[question].question_type === "mutiplechoice-multiple"
-  ) {
-    ansData = data.questions[question].possible_answers; // store answers array
-
-    var aForm = $("form.quesForms");
-    //use the each method to iterate through the answer array variable, ansData
-    $.each(ansData, function(index, value) {
-      //we create the <li></li> element with a class and an attribute and append it to <ul></ul> which we identify through the class quesLists
-      var li = $("<label/>")
-        .addClass("quesForm")
-        .appendTo(aForm);
-      //we create the <input> element with 2 attributes and append it to <li></li> and then add the anwer after the <input> element.
-      var input = $("<input/>")
-        .addClass("quesForm")
-        .attr("type", "checkbox")
-        .attr("name", "checkbox")
-        .attr("id", value.a_id)
-        .appendTo(li)
-        .after(value.caption);
-    });
+  let form = $("form.quesForms");
+  switch (data.questions[question].question_type) {
+    //show possible answers for true or false question
+    case "truefalse":
+      let liTrue = $(setTrueFalseAnswer()).appendTo(form);
+      break;
+    case "mutiplechoice-single":
+      //show possible answers for single choice question
+      ansData = data.questions[question].possible_answers;
+      setSingleQuestionOrMultipleQuestion(ansData, form, "radio");
+      break;
+    case "mutiplechoice-multiple":
+      //show possible answers for multiple choice question
+      ansData = data.questions[question].possible_answers;
+      setSingleQuestionOrMultipleQuestion(ansData, form, "checkbox");
+      break;
+    default:
+      break;
   }
-  $("input").click(function() {
-    if (data.questions[question].question_type === "mutiplechoice-multiple") {
-      var answers = document.forms[0];
+  handleUserSelectedAnswers();
+};
 
-      mulAnsSelected = [];
-      for (var i = 0; i < answers.length; i++) {
-        if (answers[i].checked) {
-          mulAnsSelected.push(answers[i].id);
-        }
-      }
-      $("input").click(function() {
-        if (
-          data.questions[question].question_type === "mutiplechoice-multiple"
-        ) {
-          if (mulAnsSelected.length === 0) {
-            $(".confirm").hide();
+let handleUserSelectedAnswers = function() {
+  $("input").click(function() {
+    switch (data.questions[question].question_type) {
+      case "mutiplechoice-multiple": //if its multiple choice answer
+        let answers = document.forms[0]; //find form label.
+        mulAnsSelected = []; // reset answers selected to empty array
+        for (let i = 0; i < answers.length; i++) {
+          if (answers[i].checked) {
+            mulAnsSelected.push(answers[i].id); //push id of answer/s selected to mulAnsSelected array
           }
         }
-      });
-    } else {
-      ansSelected = jQuery(this).attr("id"); //when an answer is clicked, set ansSelected variable to the value of id of the selected element.
-      ansSelected = Number(ansSelected);
+        $("input").click(function() {
+          if (mulAnsSelected.length === 0) {
+            // hide confirm selection button if no answers are selected
+            $(".confirm").hide();
+          }
+        });
+        break;
+      default:
+        //if question is true/false answer or single choice answer
+        ansSelected = jQuery(this).attr("id"); //when an answer is clicked, set ansSelected variable to the value of id of the selected element.
+        ansSelected = Number(ansSelected); //convert string to Number
+        break;
     }
 
     //below logic prevents users from selecting other answers when they have confirmed their answer through the enSelect boolean.
-    if (enSelect) {
-      $(".confirm").hide();
-    } else {
-      $(".confirm").show();
+    switch (enSelect) {
+      case true:
+        $(".confirm").hide();
+        break;
+      case false:
+        $(".confirm").show();
+        break;
+      default:
+        break;
     }
   });
 };
 
-var finalResults = function() {
+let setTrueFalseAnswer = function() {
+  return '<label class="quesList" ><input id=0 type="radio" name="radio">True</label><label class="quesList" ><input id=1 type="radio" name="radio">False</label>';
+};
+
+let setSingleQuestionOrMultipleQuestion = function(ansData, form, type) {
+  $.each(ansData, function(index, value) {
+    //we create the <li></li> element with a class and an attribute and append it to <ul></ul> which we identify through the class quesLists
+    let la = $("<label/>")
+      .addClass("quesForm")
+      .appendTo(form);
+
+    //we create the <input> element with 2 attributes and append it to <li></li> and then add the anwer after the <input> element.
+    let input = $("<input/>")
+      .attr("type", type)
+      .attr("name", type)
+      .attr("id", value.a_id)
+      .appendTo(la)
+      .after(value.caption);
+  });
+};
+
+let finalResults = function() {
   console.log("finalResults" + " " + ((points / 20) * 100).toFixed(0) + "%");
   resultData = $.parseJSON(
     $.ajax({
@@ -167,28 +163,29 @@ var finalResults = function() {
       async: false
     }).responseText
   );
-  pointsPerc = ((points / 20) * 100).toFixed(0);
-
-  for (var i = 0; i < resultData.results.length; i++) {
-    if (pointsPerc <= resultData.results[i].maxpoints) {
+  pointsPerc = ((points / 20) * 100).toFixed(0); //calculate points percentage
+  console.log(pointsPerc);
+  for (let i = 0; i < resultData.results.length; i++) {
+    if (
+      pointsPerc >= resultData.results[i].minpoints &&
+      pointsPerc <= resultData.results[i].maxpoints
+    ) {
       resultHandler(resultData.results[i]);
       break;
     }
   }
 };
 
-var resultHandler = function(res) {
+let resultHandler = function(res) {
   $(".quesForm").remove();
   $(".quesList").remove();
   $(".question").remove();
   $(".quesImage").attr("src", res.img);
   $(".finalResult").text(res.title);
-  $(`<p style="text-align: center">${res.message}</p>`).appendTo(
+  $(`<p class="resultMessageText">${res.message}</p>`).appendTo(
     ".resultMessage"
   );
   $(".pointsPercContiner").show();
   $("#pointsPerc").text(pointsPerc + "%");
   $(".resetGame").show();
-  //$(".resultMessage").text(res.message); //display incorrect answer text to user
-  console.log(res.message);
 };
